95a8ceef0b32e05379d912876629393c1f18412a
Gavriil Sitnikov <zuzuk666@gmail.com>
2017-04-04 20:55:38 +0300

    no-class of 1.2.9 rxjava crash fixed

--- src/main/java/ru/touchin/roboswag/core/observables/SequenceObservableExecutor.java
+++ src/main/java/ru/touchin/roboswag/core/observables/SequenceObservableExecutor.java
@@ -7,9 +7,11 @@ import android.support.annotation.Nullable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 
-import rx.Completable;
+import rx.Observable;
 import rx.Scheduler;
+import rx.Subscriber;
 import rx.Subscription;
+import rx.functions.Actions;
 import rx.schedulers.Schedulers;
 
 //TODO: errors/next/completion/single/observable/block next after first fail
@@ -19,31 +21,31 @@ public class SequenceObservableExecutor {
     private final Scheduler sendingScheduler = Schedulers.from(Executors.newSingleThreadExecutor());
 
     @NonNull
-    public Completable execute(@NonNull final Completable completable) {
+    public Observable<?> execute(@NonNull final Observable<?> completable) {
         final Task task = new Task(completable);
-        return Completable
+        return Observable
                 .create(task)
                 .doOnUnsubscribe(task::cancel);
     }
 
-    private class Task implements Completable.CompletableOnSubscribe {
+    private class Task implements Observable.OnSubscribe<Subscriber> {
 
         @NonNull
-        private final Completable completable;
+        private final Observable<?> completable;
         @Nullable
         private Subscription subscription;
 
-        public Task(@NonNull final Completable completable) {
+        public Task(@NonNull final Observable<?> completable) {
             this.completable = completable;
         }
 
         @Override
-        public void call(@NonNull final Completable.CompletableSubscriber subscriber) {
+        public void call(@NonNull final Subscriber subscriber) {
             subscription = sendingScheduler.createWorker().schedule(() -> {
                 final CountDownLatch blocker = new CountDownLatch(1);
                 final Subscription executeSubscription = completable
                         .doOnTerminate(blocker::countDown)
-                        .subscribe(subscriber::onError, subscriber::onCompleted);
+                        .subscribe(Actions.empty(), subscriber::onError, subscriber::onCompleted);
                 try {
                     blocker.await();
                 } catch (final InterruptedException exception) {
